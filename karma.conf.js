a63726c1e5299d164c13b6237d1d637b8912c171
Thodoris Greasidis <thgreasi@gmail.com>
2022-09-22 12:28:34 +0300

    Fix karma browser tests in node 18
    Change-type: patch

--- karma.conf.js
+++ karma.conf.js
@@ -4,5 +4,30 @@ const packageJSON = require('./package.json');
 module.exports = (config) => {
 	const karmaConfig = getKarmaConfig(packageJSON);
 	karmaConfig.logLevel = config.LOG_INFO;
+	// polyfill required for mockttp & balena-request
+	// the next major might not require them any more
+	karmaConfig.webpack.resolve = {
+		fallback: {
+			assert: false,
+			crypto: false,
+			fs: false,
+			path: false,
+			stream: require.resolve('stream-browserify'),
+			url: false,
+			util: false,
+			zlib: require.resolve('browserify-zlib'),
+		},
+	};
+	karmaConfig.webpack.plugins = [
+		new getKarmaConfig.webpack.ProvidePlugin({
+			// Polyfills or mocks for various node stuff
+			process: 'process/browser',
+			Buffer: ['buffer', 'Buffer'],
+		}),
+	];
+	karmaConfig.webpack.experiments = {
+		asyncWebAssembly: true,
+	};
+
 	config.set(karmaConfig);
 };
